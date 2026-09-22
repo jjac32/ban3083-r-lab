da43edc0ad3375d9d975156cd9660caa7a7c5603
Joey C <101367256+jjac32@users.noreply.github.com>
2026-09-22 12:27:00 -0500

    Create install.R
    Pre-installs DBI, RSQLite, tidyverse, dbplyr, rstudioapi, and rmarkdown.

---
+++ install.R
@@ -0,0 +1,14 @@
+install.packages(c(
+  "DBI",
+  "RSQLite",
+  "tidyverse",
+  "dbplyr",
+  "rmarkdown",
+  "knitr",
+  "rstudioapi",
+  "palmerpenguins",
+  "janitor",
+  "skimr",           # Easy summary statistics
+  "rmarkdown",       # Knitting lab reports
+  "knitr"
+))
